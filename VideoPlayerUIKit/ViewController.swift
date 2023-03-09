9b565226b6cee8bbf0558ddf8613d6f91b694a41
Muhammad Abbas <abbas@vigoroustechnologies.com>
2023-03-09 10:14:06 -0600

    Initial Commit

---
+++ VideoPlayerUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayerUIKit
+//
+//  Created by Zeeshan Suleman on 09/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
